844872f61e402bb9137c9456476612da571d7416
Ankit <ankitv.ashokaec19@gmail.com>
2025-12-21 22:36:21 +0530

    'initial'

---
+++ React.js
@@ -0,0 +1,46 @@
+React.js
+
+React.js and its features
+Lifecycle methods of React
+what is component
+what is JSX
+What is virtual dom
+Real dom vs virtual dom
+Limitions of react
+What are props in React.js
+What is state in React.js
+setState() method in React.js
+Strict mode in react js
+Functional component
+Class component
+Stateless vs Statefull component
+How to prevent re-renders in react?
+Diff between props and state
+Pure component and Pure Function
+Currying
+map() funtion
+Batching
+memo
+Refs
+forward Refs
+Portal
+Destruring prop and state
+Event Handlers
+Conditional Rendering
+Fragments
+Render-props
+Axios
+Prop drilling in React
+Context API React
+Updating Context from nested component
+Drawback of Context API
+Context.DisplayName
+What is Hoos in React
+useState hook
+useState hook with previous state, object and array
+useEffect hook
+Conditionally running useEffect
+Running useEffect only once
+useEffect to perform clean up
+useEffect with incorrect dependency
+Fetching data using useEffect
